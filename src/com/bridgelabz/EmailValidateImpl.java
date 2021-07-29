c2ace395fc8110cef220d2c3671701b9ef79effb
23Shivani <shivani.gharve.2310@gmail.com>
2021-07-29 09:58:41 +0530

    Added: Email Validation Program

---
+++ src/com/bridgelabz/EmailValidateImpl.java
@@ -0,0 +1,28 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailValidateImpl implements EmailId {
+	
+	
+	@Override
+	public void emailValidate()  {
+		Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Enter your Email id");
+        String email = sc.next();
+
+        Pattern pattern = Pattern.compile("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$");
+        Matcher matcher = pattern.matcher(email);
+        if(matcher.matches()){
+            System.out.println("Given email id is valid");
+        }else{
+            System.out.println("Given email id is not valid");
+        }
+		
+	sc.close();
+	}
+}
